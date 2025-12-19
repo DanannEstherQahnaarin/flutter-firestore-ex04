69340d2b549ae1fc22c7433b54314c527b60e8c0
Qahnaarin <deepestdark@gmail.com>
2025-12-19 21:06:10 +0900

    file add

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_firestore_ex04/firebase_options.dart';
+import 'package:flutter_firestore_ex04/pages/page_about.dart';
+import 'package:flutter_firestore_ex04/pages/page_board_list.dart';
+import 'package:flutter_firestore_ex04/pages/page_image_board_list.dart';
+import 'package:flutter_firestore_ex04/screen/main_navigation_screen.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_firestore_ex04/provider/provider_auth.dart';
 
@@ -20,7 +24,17 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
+  Widget build(BuildContext context) => MaterialApp(
+    title: 'Flutter Advanced Community',
+    theme: ThemeData(primarySwatch: Colors.blue),
+    // 초기 페이지는 홈페이지로 설정
+    initialRoute: '/',
+    routes: {
+      '/': (context) => const MainNavigationScreen(),
+      '/board': (context) => const BoardListPage(),
+      '/imageBoard': (context) => const ImageBoardListPage(),
+      '/about': (context) => const AboutPage(),
+      // TODO: 로그인, 회원가입 라우트 추가 예정
+    },
+  );
 }
---
+++ lib/screen/main_navigation_screen.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_firestore_ex04/common_widgets/common_appbar.dart';
+import 'package:flutter_firestore_ex04/common_widgets/common_navi.dart';
+import 'package:flutter_firestore_ex04/pages/page_about.dart';
+import 'package:flutter_firestore_ex04/pages/page_board_list.dart';
+import 'package:flutter_firestore_ex04/pages/page_home.dart';
+import 'package:flutter_firestore_ex04/pages/page_image_board_list.dart';
+
+class MainNavigationScreen extends StatefulWidget {
+  const MainNavigationScreen({super.key});
+
+  @override
+  State<MainNavigationScreen> createState() => _MainNavigationScreenState();
+}
+
+class _MainNavigationScreenState extends State<MainNavigationScreen> {
+  int _selectedIndex = 0;
+
+  // 4개의 메뉴 페이지 정의
+  final List<Widget> _widgetOptions = <Widget>[
+    const HomePage(),
+    const BoardListPage(),
+    const ImageBoardListPage(),
+    const AboutPage(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    appBar: buildCommonAppBar('Advanced Community'),
+    body: Center(
+      child: _widgetOptions.elementAt(_selectedIndex), // 선택된 페이지 표시
+    ),
+    bottomNavigationBar: buildCommonBottomNavigationBar(
+      currentIndex: _selectedIndex,
+      itemsMap: const {
+        '홈': Icons.home,
+        '글 게시판': Icons.article,
+        '이미지 게시판': Icons.image,
+        'About': Icons.info,
+      },
+      onTap: _onItemTapped,
+    ),
+  );
+}
